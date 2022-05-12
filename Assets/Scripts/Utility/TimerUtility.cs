cffc194a81a9573fc50b6823767cc3affe291bab
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2022-05-12 11:19:05 +0200

    added TimerUtility and Timer object

---
+++ Assets/Scripts/Utility/TimerUtility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TimerUtility
+{
+    public static Timer Create(float duration)
+	{
+		Timer t = new Timer() { duration = duration };
+		return t;
+	}
+}
+
+public class Timer
+{
+	public event Action OnTimerEnd;
+
+	public bool done => progress <= 0;
+
+	public float duration;
+	public float progress;
+
+	public void Start()
+	{
+		progress = duration;
+	}
+
+	public void Update()
+	{
+		progress -= Time.deltaTime;
+
+		if (progress <= 0)
+			OnTimerEnd?.Invoke();
+	}
+}
