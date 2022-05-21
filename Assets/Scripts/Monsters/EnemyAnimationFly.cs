0d897fa7c751a6a28df4296a3851ffbc0a8aaa4e
Lucas Rossignol <lucas.rossignol@studioblackflag.com>
2022-05-21 19:18:27 +0200

    Added animation controller for flying monster

---
+++ Assets/Scripts/Monsters/EnemyAnimationFly.cs
@@ -0,0 +1,27 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimationFly : MonoBehaviour
+{
+    public AIPath aiPath;
+    public SpriteRenderer sprite;
+
+	public void Update()
+	{
+		if (aiPath.desiredVelocity.x > 0.1f)
+			FlipRight();
+
+		else if (aiPath.desiredVelocity.x < -0.1f)
+			FlipLeft();
+	}
+
+	private void FlipLeft() => Flip(true);
+	private void FlipRight() => Flip(false);
+
+	private void Flip(bool isFlip)
+	{
+		sprite.flipX = isFlip;
+	}
+}
